52e816f81e8c50c006a74056d4484bc1c2712b52
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-04-06 08:50:32 -0400

    Update whackAMole.py

--- whackAMole.py
+++ whackAMole.py
@@ -5,44 +5,55 @@
 from ggame import *
 from random import randint
 
-#what happens when you click the mouse
-#def mouseClick(event): 
     
 def moveMole():
-    data['frames'] = 0 #reset the tp timer
+    print("hello")
+    #data['frames'] = 0 #reset the tp timer
     holeNum = randint(1,6)
+    return(holeNum)
     if holeNum==1:
         mole.x = 30
         mole.y = 50
-    elif holenum==2:
+    elif holeNum==2:
         mole.x = 200
         mole.y = 50
-    elif holenum==3:
+    elif holeNum==3:
         mole.x = 370
         mole.y = 50
-    elif holenum==4:
+    elif holeNum==4:
         mole.x = 30
         mole.y = 200
-    elif holenum==5:
+    elif holeNum==5:
         mole.x = 200
         mole.y = 200
     else:
         mole.x = 370
         mole.y = 200
-     
+
+#what happens when you click the mouse
+"""
+def mouseClick(event):
+    if **Mouse location 
+"""
+
 
 #Updating score
-    def updateScore(): 
-        data['score'] += 10
+def updateScore(): 
+    data['score'] += 10
+    data['scoreText'].destroy() #remove old writing
+    scoreBox = TextAsset('Score = '+str(data['score']))
+"""
+    def missScore(): 
+        data['score'] -= 5
         data['scoreText'].destroy() #remove old writing
         scoreBox = TextAsset('Score = '+str(data['score']))
-
+"""
 
 #Keeps track of how many frames since last teleport, and TPs it after 10 frames
 def step():
     data['frames'] +=1
-    if data['frames']%10 == 0:
-        moveMole()
+    print("test")
+    moveMole() 
 
 #Sets up and runs game 
 if __name__ == '__main__':
@@ -58,7 +69,8 @@ if __name__ == '__main__':
     
     #graphics for the graphic throne
     moleHill = CircleAsset(70, LineStyle(1,black),black)
-    mole = CircleAsset(70, LineStyle(1,black),brown)
+    moleAsset = CircleAsset(70, LineStyle(1,black),brown)
+    gague = LineAsset(50,160,blackOutline)
     
     
     Sprite(moleHill, (30,50))
@@ -67,9 +79,10 @@ if __name__ == '__main__':
     Sprite(moleHill, (30,200))
     Sprite(moleHill, (200,200))
     Sprite(moleHill, (370,200))
-    Sprite(mole)
+    Sprite(gague(0,0))
+    mole = Sprite(moleAsset, (30,50))
 
 #This activates the click part
-    #App().listenMouseEvent('click', mouseClick) 
+    App().listenMouseEvent('click', mouseClick) 
     App().run(step)
 
